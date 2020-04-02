e5ae4fdc86f9f9b0b2d900551f603049990f42da
Dewanto Krisna <dewantokrisna@gmail.com>
2020-04-02 20:13:45 +0700

    mencoba oop pada produk.php

--- produk.php
+++ produk.php
@@ -2,7 +2,30 @@
 
 class Produk
 {
+    public $judul = 'judul',
+        $penulis,
+        $penerbit,
+        $harga;
+
+    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0)
+    {
+        $this->judul = $judul;
+        $this->penulis = $penulis;
+        $this->penerbit  = $penerbit;
+        $this->harga = $harga;
+    }
+
+    public function getLabel()
+    {
+        return "$this->penulis, $this->penerbit";
+    }
 }
 
-$produk1 = new Produk();
-$produk2 = new Produk();
+// $produk1 = new Produk();
+// $produk1->judul = "Naruto";
+// $produk1->penulis = "Masashi Kishimoto";
+// $produk1->penerbit = "Shonen Jump";
+// $produk1->harga = "50000";
+
+$produk1 = new produk();
+echo $produk1->getLabel();
