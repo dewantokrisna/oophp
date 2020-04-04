322c4b5ae5f16cb3fe218227aee1e89f7e40286e
Dewanto Krisna <dewantokrisna@gmail.com>
2020-04-04 07:44:21 +0700

    belajar tentang autoload dan spl_autoload_register

---
+++ autoloading/App/Produk/Produk.php
@@ -0,0 +1,80 @@
+<?php
+
+abstract class Produk
+{
+    protected $judul = 'judul',
+        $penulis,
+        $penerbit,
+        $harga,
+        $diskon = 0;
+
+    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0)
+    {
+        $this->judul = $judul;
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+    }
+
+    public function setJudul($judul)
+    {
+        if (!is_string($judul)) {
+            throw new Exception("Judul Harus string");
+        }
+        $this->judul = $judul;
+    }
+
+    public function getLabel()
+    {
+        return "$this->penulis, $this->penerbit";
+    }
+
+
+
+    abstract public function getInfo();
+
+    public function setPenulis($penulis)
+    {
+        $this->penulis = $penulis;
+    }
+
+    public function getPenulis()
+    {
+        return $this->penulis;
+    }
+
+    public function setPenerbit($penerbit)
+    {
+        $this->penerbit = $penerbit;
+    }
+
+    public function getPenerbit()
+    {
+        return $this->penerbit = $penerbit;
+    }
+
+    public function setDiskon($diskon)
+    {
+        $this->diskon = $diskon;
+    }
+
+    public function getDiskon()
+    {
+        return $this->diskon;
+    }
+
+    public function getjudul()
+    {
+        return $this->judul;
+    }
+
+    public function setHarga($harga)
+    {
+        $this->harga = $harga;
+    }
+
+    public function getHarga()
+    {
+        return $this->harga - ($this->harga * $this->diskon / 100);
+    }
+}
